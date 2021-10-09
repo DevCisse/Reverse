25d752808cf88b23015829ba0c8fb76b7acc72e1
Hassan Salihu <45653783+DevCisse@users.noreply.github.com>
2021-10-09 15:10:08 +0100

    Finishing touches

--- js/site.js
+++ js/site.js
@@ -1,7 +1,38 @@
+//Get the string from the page
 
-//call hello world
-
-function helloWorld()
+//controller function
+function getValues()
 {
-    alert('hello world');
+  document.getElementById('alert').classList.add('invisible')
+
+  let userString =   document.getElementById('userString').value;
+
+  let revString = reverseString(userString);
+
+  displayString(revString);
+
+}
+
+//reverse the string
+function reverseString(userString)
+{
+
+  let revString =  [];
+  for (let index = userString.length -1 ; index >=0 ; index--) {
+    revString += userString[index];
+    
+  }
+  return revString;
+}
+
+
+//Display message
+
+//view Funciton
+function displayString(revString){
+  //write to the page
+document.getElementById('msg').innerHTML = `Your string reversed is ${revString}`;
+  //show the alert box
+document.getElementById('alert').classList.remove('invisible'); 
+
 }
